ab149b0789f78d92038a4873a430e65c2b61890f
Lin-Ya <fenghuiwing@outlook.com>
2018-05-26 19:32:00 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Vue-Resumer3/dist/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/Vue-Resumer3/dist/',
 
     /**
      * Source Maps
